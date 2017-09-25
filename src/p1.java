5ea26bc59e213e1e89e315e3ff5c0d541a8e3c71
BlackBloodE <106021032@live.asia.edu.tw>
2017-09-25 15:20:49 +0800

    模組1練習題

---
+++ src/p1.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class p1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //使用者輸入(背起來!)
+        int a = sc.nextInt(); //
+        double b = (double) a ;
+        System.out.print(b*1.6);
+    }
+}
